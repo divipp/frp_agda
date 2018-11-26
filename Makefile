aff11a16c873a99ac6d8505796d95ac0f5ae58a8
Peter Divianszky <divipp@gmail.com>
2018-11-26 11:16:49 +0100

    use the new --js-output and --js-externals flag of the Agda compiler and try to generate as small JS code as possible

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 
 all.js: rts.js jAgda.Reactive.js
-	cat rts.js jAgda.Prelude.js jAgda.Reactive.js > $@
+	cat rts.js jAgda.Reactive.js > $@
 
-jAgda.Reactive.js: Reactive.agda Prelude.agda
-	agda --js --js-optimize --no-main Reactive.agda
+jAgda.Reactive.js: Reactive.agda Prelude.agda externals.txt
+	agda --js --js-optimize --js-minify --js-output=jAgda.Reactive.js --js-externals=externals.txt --no-main Reactive.agda
+
 
 .PHONEY: run
 run: all.js
--- rts.js
+++ rts.js
@@ -22,33 +22,24 @@ const performance_warnings = true;
 //////////////////////////////////////////////////////////////////////////////////////////////
 // functions called from Agda code
 
-// `exports` is an Object holding all Agda generated JS code and the primitive functions
-// `exports` remains contant after the browser loads the js source files
-const exports = {};
-
-// we assume that modules define distinct global names so no module name spacing is needed
-// (every module uses `exports`)
-const require = _ => exports;
-
-const mkBool = b => b ? exports["Bool"]["true"] : exports["Bool"]["false"];
+const mkBool = b => b ? T/*true*/ : F/*false*/;
 
 // These primitive functions are directly called from Agda generated JS code
-// `exports.primIntegerFromString("0")` is also used to fill in erased arguments by Agda
-exports.primIntegerFromString           = x => parseInt(x, 10);
-exports.primSeq                         = (x, y) => y;
-exports.uprimIntegerPlus                = (x, y) => x + y;
-exports.uprimIntegerMinus               = (x, y) => x - y;
-exports.uprimIntegerMultiply            = (x, y) => x * y;
-exports.uprimIntegerEqual               = (x, y) => mkBool(x === y);
-exports.uprimIntegerGreaterOrEqualThan  = (x, y) => mkBool(x >= y);
-exports.uprimIntegerLessThan            = (x, y) => mkBool(x < y);
+const S/*primSeq*/      = (x, y) => y;
+const I/*mkInt*/        = x => parseInt(x, 10);
+const IPlus  = (x, y) => x + y;
+const IMinus = (x, y) => x - y;
+const IMul   = (x, y) => x * y;
+const IEq    = (x, y) => mkBool(x === y);
+const IGE    = (x, y) => mkBool(x >= y);
+const ILT    = (x, y) => mkBool(x < y);
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 // utility functions
 
 // marshalling function from Agda Vec to JS array
 const fromVec = (vec, ch) => vec (
-  [ () => ch
+  [ _ => ch
   , (str, xs) => { ch.push(str); return fromVec(xs, ch); }
   ]);
 
@@ -60,10 +51,10 @@ const createElem = x => {
   for (const key in x[1]) {
     const el = x[1][key];
     switch (key) {
-      case "children":
+      case "C"/*children*/:
         for (const i in el) {res.appendChild(createElem(el[i]));};
         break;
-      case "attrs":
+      case "A"/*attributes*/:
         for (const i in el) {res.setAttribute(i, el[i]);};
         break;
       default:
@@ -77,8 +68,8 @@ const createElem = x => {
 //////////////////////////////////////////////////////////////////////////////////////////////
 // conversion of Agda widget descriptions to DOM elements
 
-const setEnabled = (a, obj) => Object.assign(obj, a ([() => {}, () => ({disabled: true})]));
-const setValidity = a => a ([() => [], () => ["invalid"]]);
+const setEnabled = (a, obj) => Object.assign(obj, a ([_ => {}, _ => ({disabled: true})]));
+const setValidity = a => a ([_ => [], _ => ["invalid"]]);
 
 // pure marshalling function from Agda Widget to a DOM element description
 // `dir` is either 'horizontal' or 'vertical'; it is used only in the "Container" case
@@ -86,28 +77,28 @@ const fromWidget = (dir, widget) => widget (
   [ (en, str) =>
      [ "button"
      , { textContent: str
-       , attrs: setEnabled(en, {input: "button", onclick: "click_event(this)"})}]
+       , A: setEnabled(en, {input: "button", onclick: "clickE(this)"})}]
   , (en, checked) =>
      [ "input"
-     , { checked: checked([() => false, () => true])
-       , attrs: setEnabled(en, {type: "checkbox", onchange: "toggle_event(this)"})}]
+     , { checked: checked([_ => false, _ => true])
+       , A: setEnabled(en, {type: "checkbox", onchange: "toggleE(this)"})}]
   , (_, en, opts, sel) =>
      [ "select"
-     , { children:      fromVec(opts, []).map(str => ["option", {textContent: str}])
-       , selectedIndex: exports["finToNat"](sel)
-       , attrs:         setEnabled(en, {oninput: "select_event(this)"})}]
+     , { C:      fromVec(opts, []).map(str => ["option", {textContent: str}])
+       , selectedIndex: finToNat(sel)
+       , A:         setEnabled(en, {oninput: "selectE(this)"})}]
   , (en, size, name, str, val) =>
      [ "input"
      , { value: str
        , classList: setValidity(val)
-       , attrs: setEnabled(en, {type: "text", oninput: "entry_event(this)", size: size, name: name})}]
+       , A: setEnabled(en, {type: "text", oninput: "entryE(this)", size: size, name: name})}]
   , str => [ "span", {textContent: str, classList: ["label"]} ]
-  , () => [ "span", {} ]
+  , _ => [ "span", {} ]
   , (newd, w1, w2) => {
-      const newdir = newd([() => "horizontal", () => "vertical"]);
+      const newdir = newd([_ => "horizontal", _ => "vertical"]);
       return [ "div"
              , { classList: [dir]
-               , children:  [fromWidget(newdir, w1), fromWidget(newdir, w2)]}];
+               , C:  [fromWidget(newdir, w1), fromWidget(newdir, w2)]}];
     }
   ]);
 
@@ -116,8 +107,8 @@ const fromWidget = (dir, widget) => widget (
 
 // `updateWidget` applies the `ch` change on the `el` DOM element
 const updateWidget = (el, ch) => ch (
-  [ () => { el.checked = not(el.checked); }
-  , null
+  [ _ => { el.checked = !el.checked; }
+  , 0
   , str => {
       if (performance_warnings && str === el.textContent) { console.log("performance waning: label text was setted", el, str); };
       el.textContent = str;}
@@ -128,7 +119,7 @@ const updateWidget = (el, ch) => ch (
       if (performance_warnings && idx === el.selectedIndex) { console.log("performance waning: option was selected", el, idx); };
       el.selectedIndex = idx;}
   , _ => el.getAttribute("disabled") ? el.removeAttribute("disabled") : el.setAttribute("disabled", true)
-  , () => { el.classList.toggle("invalid"); }
+  , _ => { el.classList.toggle("invalid"); }
   , w => {
       if (performance_warnings) { console.log("performance log: replaceBy"); };
       const parent = el.parentNode;
@@ -136,7 +127,7 @@ const updateWidget = (el, ch) => ch (
       parent.replaceChild(createElem(fromWidget(dir, w)), el);}
   , w => { updateWidget(el.childNodes[0],w); }
   , w => { updateWidget(el.childNodes[1],w); }
-  , null, null, null, null
+  , 0, 0, 0, 0
   , (w1, w2) => { updateWidget(el,w1); updateWidget(el,w2); }
   ]);
 
@@ -148,25 +139,25 @@ const updateWidget = (el, ch) => ch (
 const trans_event = (el, fu) => {
   const p = el.parentNode;
   return p == domRoot ? fu
-         : trans_event(p, exports["WidgetEdit"][(p.childNodes[0] === el ? "modLeft" : "modRight")](fu));
+         : trans_event(p, (p.childNodes[0] === el ? ML/*modLeft*/ : MR/*modRight*/)(fu));
 };
 
 // `handle_event` handles an event `inn` relative to the `el` DOM element
-const handle_event = (el, inn) => state(x1 => x1)(exports["Maybe"]["just"](trans_event(el, inn)))(x1 => x1)(
+const handle_event = (el, inn) => state(x1 => x1)(J/*just*/(trans_event(el, inn)))(x1 => x1)(
   (p1, p2) => {
-    p1([() => {}, dw => { updateWidget(domRoot.childNodes[0],dw); }]);
+    p1([_ => {}, dw => { updateWidget(domRoot.childNodes[0],dw); }]);
     state = p2;
   });
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 // the functions which are called directly by the browser
 
-const click_event  = el => handle_event(el, exports["WidgetEdit"]["click"]);
-const toggle_event = el => handle_event(el, exports["WidgetEdit"]["toggle"]);
-const entry_event  = el => handle_event(el, exports["WidgetEdit"]["setEntry"](el.value));
-const select_event = el => handle_event(el, exports["WidgetEdit"]["select"](exports["natToFin"](el.selectedIndex)));
+const clickE  = el => handle_event(el, click);
+const toggleE = el => handle_event(el, toggle);
+const entryE  = el => handle_event(el, SE/*setEntry*/(el.value));
+const selectE = el => handle_event(el, select(natToFin(el.selectedIndex)));
 
-window.onload = () => exports["main"](
+window.onload = _ => main(
   (p1, p2) => {
     state = p2;
     domRoot.appendChild(createElem(fromWidget("vertical", p1)));
